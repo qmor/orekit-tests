52250323e1dfa6c926737191b90c2f6c7b4e33e3
Олег Мороз <oleg.moroz@mcc.vniiem.ru>
2020-01-26 10:28:31 +0300

    Небольшойфикс софта
    Модель атмосферы ДМА2004 (но даже не проверена)

--- src/ru/vniiem/orekit/TestProgramm.java
+++ src/ru/vniiem/orekit/TestProgramm.java
@@ -27,8 +27,6 @@ import org.orekit.models.earth.atmosphere.data.MarshallSolarActivityFutureEstima
 import org.orekit.orbits.CartesianOrbit;
 import org.orekit.orbits.Orbit;
 import org.orekit.propagation.SpacecraftState;
-import org.orekit.propagation.events.AbstractDetector;
-import org.orekit.propagation.events.EventDetector;
 import org.orekit.propagation.events.NodeDetector;
 import org.orekit.propagation.numerical.NumericalPropagator;
 import org.orekit.propagation.sampling.OrekitFixedStepHandler;
@@ -62,9 +60,9 @@ public class TestProgramm {
 		OneAxisEllipsoid earth = new OneAxisEllipsoid(Constants.WGS84_EARTH_EQUATORIAL_RADIUS, Constants.WGS84_EARTH_FLATTENING, ITRFFrame);
 		Frame inertialFrame = FramesFactory.getEME2000();
 		TimeScale utc = TimeScalesFactory.getUTC();
-		AbsoluteDate initialDate = new AbsoluteDate(2020, 01, 19, 21, 24, 26.142101, utc);
+		AbsoluteDate initialDate = new AbsoluteDate(2020, 01, 24, 02, 50, 42.630425, utc);
 
-		PVCoordinates coordsECEF =  new PVCoordinates(new Vector3D(-5213.56894604261*1e3,-4503.69735468414*1e3,2.07433845478278E-06*1e3) , new Vector3D(-0.96642490779325*1e3,1.13339627279594*1e3,7.54405377145847*1e3));
+		PVCoordinates coordsECEF =  new PVCoordinates(new Vector3D(-4309.11566316175*1e3,5376.31103200996*1e3,1.41441272603228E-06*1e3) , new Vector3D(1.16764973146406*1e3,0.923459903868841*1e3,7.54353453883883*1e3));
 		PVCoordinates coordsECI =  ITRFFrame.getTransformTo(inertialFrame, initialDate).transformPVCoordinates(coordsECEF);
 		GeodeticPoint gp =earth.transform(coordsECEF.getPosition(), ITRFFrame, initialDate);
 		Orbit initialOrbit  = new CartesianOrbit(coordsECI, inertialFrame, initialDate, org.orekit.utils.Constants.WGS84_EARTH_MU);
@@ -86,14 +84,14 @@ public class TestProgramm {
 		// set up space dynamics propagator
 		NumericalPropagator propagator = new NumericalPropagator(integrator);
 	
-		SpacecraftState state = new SpacecraftState(initialOrbit,600);
+		SpacecraftState state = new SpacecraftState(initialOrbit,550);
 		
 		
 		MarshallSolarActivityFutureEstimation msafe =
                 new MarshallSolarActivityFutureEstimation("Sep2015F10\\.txt", MarshallSolarActivityFutureEstimation.StrengthLevel.STRONG);
 		
 		Atmosphere atmosphere = new DTM2000(msafe,  sun,earth);
-		DragSensitive dragsens = new IsotropicDrag(2,3);
+		DragSensitive dragsens = new IsotropicDrag(1.0,-0.000254397782639841*10e3);
 		DragForce df = new DragForce(atmosphere, dragsens);
 		propagator.addForceModel(df);
 		propagator.addForceModel((new ThirdBodyAttraction(CelestialBodyFactory.getSun())));
@@ -102,7 +100,7 @@ public class TestProgramm {
 		propagator.setInitialState(state);
 		NodeDetector nodeDetector =  new NodeDetector(initialOrbit, ITRFFrame) 
 		{
-			int orbitNumber = 41583;
+			int orbitNumber = 5969;
 		@Override
 		public Action eventOccurred(SpacecraftState s, boolean increasing) {
 			if (increasing)
@@ -110,6 +108,8 @@ public class TestProgramm {
 			System.out.println(orbitNumber++);
 			System.out.println("ИСК:"+s.getPVCoordinates());
 			System.out.println("ГСК:"+s.getPVCoordinates(ITRFFrame));
+			System.out.println(String.format("SMA %f I %f", s.getA() , Math.toDegrees(s.getI()) ));
+			System.out.println(String.format("Period %f E %f", s.getKeplerianPeriod()/60.0 , Math.toDegrees(s.getE()) ));
 			}
 
 			return Action.CONTINUE;
@@ -123,11 +123,11 @@ public class TestProgramm {
 			
 			@Override
 			public void handleStep(SpacecraftState currentState, boolean isLast) {
-				System.out.println(currentState.getPVCoordinates());
+				//System.out.println(currentState.getPVCoordinates());
 				
 			}
 		});
-		state =  propagator.propagate(initialDate, new AbsoluteDate(2020, 01, 22, 23, 30, 00.000, utc));
+		state =  propagator.propagate(initialDate, new AbsoluteDate(2020, 01, 25, 23, 30, 00.000, utc));
 		
 	
 	
